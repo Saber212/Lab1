a435b2229b533991a2bfbd07656dd4ceb8bdbd82
Saber <nat14ss1@student.lu.se>
2020-11-03 20:05:57 +0100

    Task 4

---
+++ Task4.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std; 
+int main(){
+    string q1 = "What is your name?\n";
+    string answer; 
+    int number; 
+
+    cout << q1; 
+    cin >> answer;
+    cout << "Okay "<<answer<<". How old are you?\n";
+    cin >> number; 
+    cout << "What is your gender?\n"; 
+    cin >> answer;
+    cout << "Are you married\n"; 
+    cin >> answer; 
+    cout << "How long are you?\n"; 
+    cin>> number;
+}
