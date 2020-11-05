25b7622d9cd21259d15879fe0e94b4394a08a587
Saber <nat14ss1@student.lu.se>
2020-11-05 16:02:11 +0100

    Prime number

--- primeNumber.cpp
+++ primeNumber.cpp
@@ -1,29 +1,34 @@
 # include <iostream> 
 using namespace std; 
 int main(){
-    int x,y,n; 
+    int x, y,n, b; 
     bool isPrime = true; 
-
-    cout << "Enter a number: "; 
-    
-    for(cin>>x; x<1000; ++x){
-        if(x==1||x==0){
-            isPrime = false;
-            cout<<"You should enter a number larger than 0 and 1."<<endl;
-            break;
-        }
-        for(y=2; y<1000; y++){
-            if(x!=y){            
-                if(x%y==0){
-                isPrime = false; 
-                break;
-                }
-            }
-            else{
-                cout<<x<<endl;
-            }
-        }
-        
+    cout<< "Enter a final number:\n"<<"n = ";
+    cin>>n;
+    cout << "Enter a start number:\n"<<"x = "; 
+    cin >> b; 
+    if(n<b){
+        cout<< "ERROR! n must be larger than b!"<<endl;
     }
-    
+    for(x=b; x<n; ++x){
+        if(x>n){
+            cout << "ERROR! n must be larger than x"<<endl;
+        }
+            if(x==1||x==0){
+                isPrime = false;
+                cout<<"You should enter a number larger than 0 and 1."<<endl;
+                break;
+        }
+           for(y=2; y<n; y++){
+                if(x!=y){            
+                    if(x%y==0){
+                    isPrime = false; 
+                    break;
+                        }
+                }
+                else{
+                    cout<<x<<endl;
+                    }
+            }
+        }
 }
