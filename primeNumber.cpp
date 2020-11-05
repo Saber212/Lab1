5f5073381da31c2c343134c1c170415a1adf0fb7
Saber <nat14ss1@student.lu.se>
2020-11-05 15:34:48 +0100

    Prime Number

--- primeNumber.cpp
+++ primeNumber.cpp
@@ -6,12 +6,13 @@ int main(){
 
     cout << "Enter a number: "; 
     
-    for(cin>>x; x<100; ++x){
+    for(cin>>x; x<1000; ++x){
         if(x==1||x==0){
             isPrime = false;
+            cout<<"You should enter a number larger than 0 and 1."<<endl;
             break;
         }
-        for(y=2; y<100; y++){
+        for(y=2; y<1000; y++){
             if(x!=y){            
                 if(x%y==0){
                 isPrime = false; 
