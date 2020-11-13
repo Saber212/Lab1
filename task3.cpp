bfa8d3f2950718ddefe85d8a096281b3ec79fa1a
Saber <nat14ss1@student.lu.se>
2020-11-13 19:48:20 +0100

    List

--- task3.cpp
+++ task3.cpp
@@ -13,7 +13,6 @@ int main(){
         n = n + 1; 
         cout << "Hello World\n";
     } while (n<10); // at the end we writie the condition in the while loop 
-    
     //using a forloop
     int y  = 0; // defining a integer 0
     for(y; y<10; y = y+1){ // using a forloop with condition that y should be less than 10
